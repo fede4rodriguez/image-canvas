8b3c59ae0f9fac239634c466fb505e4a1c8845df
Fede Rodriguez <tec.federodriguez@gmail.com>
2023-08-19 15:50:15 -0300

    Add taildwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import ImageCanvas from "./components/ImageCanvas";
 import { ANALYSIS } from "./data/analysis";
 
-
 function App() {
 
   const { image_url, detections } = ANALYSIS;
 
   return (
     <>
-      <h1>Image canvas</h1>
-      
-      <ImageCanvas image_url= {image_url} detections={detections} />
+      <header className="flex items-center">      
+        <h1 className="text-xxl">Image <span className="text-blue-400">canvas</span></h1>
+      </header>
+      <main>
+
+        <ImageCanvas image_url= {image_url} detections={detections} />
+      </main>
     </>
   )
 }
